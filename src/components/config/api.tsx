80e2f938c66aac84be56d27f8a2de7b2be3461d0
weak0 <maciej.gorzela89@gmail.com>
2023-05-22 13:27:30 +0200

    api fix

--- src/components/config/api.tsx
+++ src/components/config/api.tsx
@@ -2,6 +2,6 @@ export const options = {
     method: 'GET',
     headers: {
       accept: 'application/json',
-      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4OWRlZWEzYzhkMzdiYjY1YjE3YjBhY2EyNTVmOTI4YSIsInN1YiI6IjY0NDU1YmY5YTA2ZWZlMGZjOGE0NTVmYiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.GA1I3q8kERFLYGRj9-noOuTxV4Qx0z-g0hvG4PVMQ94'
+      Authorization: import.meta.env.VITE_TOKEN
     }
   };
